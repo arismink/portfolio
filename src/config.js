fe609bec3a3d99e3fb805a2c10640fd9080318e1
arismink <krismina.la@gmail.com>
2022-06-03 13:27:32 -0600

    update about me

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'krismina.la@gmail.com',
 
   socialMedia: [
     {
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/krismina',
     },
     {
       name: 'Codepen',
